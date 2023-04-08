2f88a457e9156a844f6d4b713a58583ab3c7a78c
Carlos Chamizo Cano <49963607+chamix0@users.noreply.github.com>
2023-04-08 13:11:31 +0200

    good camera collisions

--- Assets/Scripts/Player/Camera/CameraController.cs
+++ Assets/Scripts/Player/Camera/CameraController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class CameraController : MonoBehaviour
 {
@@ -10,12 +11,20 @@ public class CameraController : MonoBehaviour
     [SerializeField] private CinemachineFreeLook thirdPersonCamera;
     private float rotationValueY, targetRotationY, tY = 0;
     private float rotationValueX, targetRotationX;
-
     private float originalYSpeed, originalXSpeed;
-
 
     public float rotateStepY = 0.1f, angleStepX = 15f;
     private bool updateValueY = false, updateValueX = false;
+
+    //variables
+    private float currentY;
+    private float currentX;
+
+    //collision
+    private CinemachineCameraOffset cinemachineCameraOffset;
+    [SerializeField] private LayerMask collisionLayers;
+    private float originalOffset, originalDistance;
+    public float offset;
 
 
     void Start()
@@ -25,6 +34,9 @@ public class CameraController : MonoBehaviour
         rotateStepY = 0.25f;
         originalXSpeed = thirdPersonCamera.m_XAxis.m_MaxSpeed;
         originalYSpeed = thirdPersonCamera.m_YAxis.m_MaxSpeed;
+        cinemachineCameraOffset = thirdPersonCamera.transform.gameObject.GetComponent<CinemachineCameraOffset>();
+        originalOffset = cinemachineCameraOffset.m_Offset.z;
+        originalDistance = Vector3.Distance(thirdPersonCamera.transform.position, thirdPersonCamera.LookAt.position);
     }
 
     // Update is called once per frame
@@ -37,13 +49,32 @@ public class CameraController : MonoBehaviour
 
         if (updateValueX)
         {
-            UpdateRotateCameraX();
+            thirdPersonCamera.m_XAxis.Value = UpdateRotateCameraX();
+        }
+
+        CameraCollisionCheck();
+    }
+
+    private void CameraCollisionCheck()
+    {
+        RaycastHit hit;
+        float distance = Vector3.Distance(thirdPersonCamera.transform.position, thirdPersonCamera.LookAt.position);
+        if (Physics.Raycast(thirdPersonCamera.LookAt.position,
+                thirdPersonCamera.transform.position - thirdPersonCamera.LookAt.position, out hit, originalDistance
+                , collisionLayers))
+        {
+            cinemachineCameraOffset.m_Offset.z = originalOffset + Mathf.Abs(distance - hit.distance) + offset;
+        }
+        else
+        {
+            cinemachineCameraOffset.m_Offset.z = originalOffset;
         }
     }
 
     public void RotateYClockwise()
     {
         targetRotationY = thirdPersonCamera.m_YAxis.Value - rotateStepY;
+        currentY = thirdPersonCamera.m_YAxis.Value;
         updateValueY = true;
         tY = 0.0f;
     }
@@ -51,6 +82,7 @@ public class CameraController : MonoBehaviour
     public void RotateYCounterClockwise()
     {
         targetRotationY = thirdPersonCamera.m_YAxis.Value + rotateStepY;
+        currentY = thirdPersonCamera.m_YAxis.Value;
         updateValueY = true;
         tY = 0.0f;
     }
@@ -58,18 +90,21 @@ public class CameraController : MonoBehaviour
     public void RotateXClockwise()
     {
         targetRotationX = SnapPosition(thirdPersonCamera.m_XAxis.Value + angleStepX);
+        currentX = thirdPersonCamera.m_XAxis.Value;
         updateValueX = true;
     }
 
     public void RotateXCustom(float angle)
     {
         targetRotationX = SnapPosition(angle);
+        currentX = thirdPersonCamera.m_XAxis.Value;
         updateValueX = true;
     }
 
     public void RotateYCustom(float value)
     {
         targetRotationY = Mathf.Clamp(value, 0, 1);
+        currentY = thirdPersonCamera.m_YAxis.Value;
         tY = 0;
         updateValueY = true;
     }
@@ -77,21 +112,21 @@ public class CameraController : MonoBehaviour
     public void RotateXCounterClockwise()
     {
         targetRotationX = SnapPosition(thirdPersonCamera.m_XAxis.Value - angleStepX);
+        currentX = thirdPersonCamera.m_XAxis.Value;
         updateValueX = true;
     }
 
     private float UpdateRotateCameraY()
     {
-        rotationValueY = Mathf.Lerp(thirdPersonCamera.m_YAxis.Value, targetRotationY, tY);
+        currentY = Mathf.Lerp(currentY, targetRotationY, tY);
         tY += 1f * Time.deltaTime;
-
         if (tY > 1.0f)
         {
             tY = 1.0f;
             updateValueY = false;
         }
 
-        return rotationValueY;
+        return currentY;
     }
 
     public CinemachineFreeLook GetThirdPErsonCamera()
@@ -111,16 +146,19 @@ public class CameraController : MonoBehaviour
         thirdPersonCamera.m_YAxis.m_MaxSpeed = 2;
     }
 
-    private void UpdateRotateCameraX()
+    private float UpdateRotateCameraX()
     {
-        thirdPersonCamera.m_XAxis.Value = Quaternion
-            .Lerp(Quaternion.Euler(0, thirdPersonCamera.m_XAxis.Value, 0),
+        // thirdPersonCamera.m_XAxis.Value;
+        currentX = Quaternion
+            .Lerp(Quaternion.Euler(0, currentX, 0),
                 Quaternion.Euler(0, targetRotationX, 0), 5 * Time.deltaTime).eulerAngles.y;
 
-        if (Mathf.Abs(thirdPersonCamera.m_XAxis.Value - targetRotationX) < 0.1f)
+        if (Mathf.Abs(currentX - targetRotationX) < 0.1f)
         {
             updateValueX = false;
         }
+
+        return currentX;
     }
 
     float SnapPosition(float input)
